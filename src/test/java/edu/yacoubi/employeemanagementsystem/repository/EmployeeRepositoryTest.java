7fdfe5b7e780b0440243e7f7d773dbea76f7fb9c
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-08-02 21:15:24 +0200

    added test to find employee by gender

--- src/test/java/edu/yacoubi/employeemanagementsystem/repository/EmployeeRepositoryTest.java
+++ src/test/java/edu/yacoubi/employeemanagementsystem/repository/EmployeeRepositoryTest.java
@@ -1,4 +1,30 @@
+package edu.yacoubi.employeemanagementsystem.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class EmployeeRepositoryTest {
-  
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    void countByGender() {
+        assertEquals(employeeRepository.findAll().stream()
+                    .filter(employee -> employee.getGender().equals("Female"))
+                    .count(),
+            employeeRepository.countByGender("Female"));
+
+        assertEquals(employeeRepository.findAll().stream()
+                .filter(employee -> employee.getGender().equals("Male"))
+                .count(),
+                employeeRepository.countByGender("Male"));
+
+
+        //Long cMale = employeeRepository.countByGender("Male");
+        //System.out.println("Male : " + cMale);
+    }
 }
