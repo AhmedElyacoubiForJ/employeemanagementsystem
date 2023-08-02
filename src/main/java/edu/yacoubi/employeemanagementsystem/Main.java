ac3e2533398f0c57e1b3e9236d10f31a89637e7e
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-08-03 00:29:15 +0200

    added test for repository method counterGroupByGender() in Main class

--- src/main/java/edu/yacoubi/employeemanagementsystem/Main.java
+++ src/main/java/edu/yacoubi/employeemanagementsystem/Main.java
@@ -1,13 +1,32 @@
 package edu.yacoubi.employeemanagementsystem;
 
+import edu.yacoubi.employeemanagementsystem.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class Main implements CommandLineRunner {
+
+	@Autowired
+	private EmployeeRepository repository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		// 1. How many male and female employees are there in the organization?
+		testCountGroupByMaleFemale();
+	}
+
+	private void testCountGroupByMaleFemale() {
+		repository
+				.counterGroupByGender()
+				.forEach(gCounter -> {
+					System.out.println(gCounter);
+				});
+	}
 }
