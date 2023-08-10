4bde915efc5a8b4eac9c15f429ad0205c846ae89
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-08-10 11:27:45 +0200

    added readme.md

--- some.sql
+++ some.sql
@@ -1,22 +1,27 @@
 -- select AVG(salary)
 -- from employee
 
+-- 1. average salary per department
 select AVG(salary) salary_average, department
 from employee
 group by department;
 
+-- 1.1 test 1
 select AVG(salary) salary_average_account_and_finance
 from employee
 where department = 'Account And Finance';
 
+-- 1.2 test 1
 select AVG(salary) salary_average_hr
 from employee
 where department = 'HR';
 
+-- 1.3 test 1
 select AVG(salary) salary_average_infrastructure
 from employee
 where department = 'Infrastructure';
 
+-- 2. average salary per department ordered
 select AVG(salary) salary_average, department
 from employee
 group by department
@@ -27,26 +32,30 @@ from employee
 group by department
 order by salary_average desc limit 1;
 
+-- 2. average salary per department, gender
 select AVG(salary) salary_average, department, gender
 from employee
 group by department, gender;
 
+-- 2.1 test 2
 select AVG(salary) salary_average_male, department
 from employee
 where gender = 'Male'
 group by department;
 
+-- 2.2 test 2
 select AVG(salary) salary_average_female, department
 from employee
 where gender = 'Female'
 group by department;
 
--- number of male per department
+-- 3. number of male per department
 select count(*) count_male, department
 from employee
 where gender = 'Male'
 group by department;
 
+-- 3.1 group filter
 select count(*) count_gender_male, department
 from employee
 where gender = 'Male'
