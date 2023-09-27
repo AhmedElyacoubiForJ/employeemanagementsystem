8c4b004bddd0be12ab28996152403781214018f2
A. El Yacoubi education <sihmed.elyacoubi@gmail.com>
2023-09-27 21:31:49 +0200

    added add new employee endpoint & it's view form

--- src/main/java/edu/yacoubi/employeemanagementsystem/controller/EmployeeController.java
+++ src/main/java/edu/yacoubi/employeemanagementsystem/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import edu.yacoubi.employeemanagementsystem.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -21,5 +23,19 @@ public class EmployeeController {
         List<Employee> employeesList = repository.findAll();
         modelAndView.addObject("employees", employeesList);
         return modelAndView;
+    }
+
+    @GetMapping("/addEmployeeForm")
+    public ModelAndView addEmployeeForm() {
+        ModelAndView modelAndView = new ModelAndView("add-employee-form");
+        Employee employee = new Employee();
+        modelAndView.addObject("employee", employee);
+        return modelAndView;
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@ModelAttribute Employee employee) {
+        repository.save(employee);
+        return "redirect:/showEmployees";
     }
 }
